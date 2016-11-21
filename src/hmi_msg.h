0ff085249718778c05550141e69df7f06d6d1760
taavi <taavi.tilk@gmail.com>
2016-11-21 20:39:03 +0200

    labor03 osa A

---
+++ src/hmi_msg.h
@@ -0,0 +1,13 @@
+#ifndef _HMI_MSG_H_
+#define _HMI_MSG_H_
+#define STUD_NAME "Taavi Tilk" /*jutumärkide vahele enda nimi*/
+
+const char *nameMonth[]={"January",
+			"Februar",
+			"March",
+			"April",
+			"May",
+			"June"	
+			};
+
+#endif /*_HMI_MSG_H_ endif on see*/
--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#define __ASSERT_USE_STDERR
-#include <assert.h>
+#include <string.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
+#include "print_helper.h"
+#include "hmi_msg.h"
 
 #define BLINK_DELAY_MS 100
 
@@ -12,37 +13,42 @@ void main (void)
     /* Set pin 3 of PORTA for output */
     DDRA |= _BV(DDA3);
     /* Init error console as stderr in UART3 and print user code info */
-    uart3_initialize
-    ();
+    uart0_initialize();
+    uart3_initialize();
+    stdin = stdout = &uart0_io;
     stderr = &uart3_out;
     fprintf(stderr, "Version: %s built on: %s %s\n",
             GIT_DESCR, __DATE__, __TIME__);
     fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
     /* End UART3 init and info print */
-    /* Test assert - REMOVE IN FUTURE LABS */
-    char *array;
-    uint32_t i = 1;
-    extern int __heap_start, *__brkval;
-    int v;
-    array = malloc( i * sizeof(char));
-    assert(array);
-    /* End test assert */
+
+    fprintf(stdout, STUD_NAME "\n");
+
+    /* prindib acii tabeli */
+    print_ascii_tbl(stdout);
+
+    unsigned char ascii[128]={0};
+    for (unsigned char i=0; i <128; i++){
+	ascii[i]=i;
+	}	
+    print_for_human(stdout, ascii, 128);
 
     while (1) {
         /* Set pin 3 high to turn LED on */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
-        /* Test assert - REMOVE IN FUTURE LABS */
-        /*
-         * Increase memory allocated for array by 100 chars
-         * until we have eaten it all and print space between Stack and Heap.
-         * Thats how assert works in run-time.
-         */
-        array = realloc( array, (i++ * 100) * sizeof(char));
-        fprintf(stderr, "%d ",
-                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
-        assert(array);
-        /* End test assert */
+
+	char inBuf = ' ';
+	fprintf(stdout, "Enter Month mane first letter >");
+	fscanf(stdin, "%c", &inBuf);
+	fprintf(stdout, "%c\n", inBuf);
+
+	for (int i = 0; i < 6; i++){
+		if(!strncmp(&inBuf, nameMonth[i], 1)){
+		fprintf(stdout, "%s\n", nameMonth[i]);
+		}
+	}
+
         /* Set pin 3 low to turn LED off */
         PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
