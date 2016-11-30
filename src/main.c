d823c4ecd7c79a3b490e1bf2671f118a14839af3
Taavi Tilk <taavi.tilk@gmail.com>
2016-11-30 12:01:34 +0200

    lisatud bin kataloog

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <string.h>
+#define __ASSERT_USE_STDERR
+#include <assert.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "uart.h"
-#include "print_helper.h"
-#include "hmi_msg.h"
 
 #define BLINK_DELAY_MS 100
 
@@ -13,42 +12,37 @@ void main (void)
     /* Set pin 3 of PORTA for output */
     DDRA |= _BV(DDA3);
     /* Init error console as stderr in UART3 and print user code info */
-    uart0_initialize();
-    uart3_initialize();
-    stdin = stdout = &uart0_io;
+    uart3_initialize
+    ();
     stderr = &uart3_out;
     fprintf(stderr, "Version: %s built on: %s %s\n",
             GIT_DESCR, __DATE__, __TIME__);
     fprintf(stderr, "avr-libc version: %s\n", __AVR_LIBC_VERSION_STRING__);
     /* End UART3 init and info print */
-
-    fprintf(stdout, STUD_NAME "\n");
-
-    /* prindib acii tabeli */
-    print_ascii_tbl(stdout);
-
-    unsigned char ascii[128]={0};
-    for (unsigned char i=0; i <128; i++){
-	ascii[i]=i;
-	}	
-    print_for_human(stdout, ascii, 128);
+    /* Test assert - REMOVE IN FUTURE LABS */
+    char *array;
+    uint32_t i = 1;
+    extern int __heap_start, *__brkval;
+    int v;
+    array = malloc( i * sizeof(char));
+    assert(array);
+    /* End test assert */
 
     while (1) {
         /* Set pin 3 high to turn LED on */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
-
-	char inBuf = ' ';
-	fprintf(stdout, "Enter Month mane first letter >");
-	fscanf(stdin, "%c", &inBuf);
-	fprintf(stdout, "%c\n", inBuf);
-
-	for (int i = 0; i < 6; i++){
-		if(!strncmp(&inBuf, nameMonth[i], 1)){
-		fprintf(stdout, "%s\n", nameMonth[i]);
-		}
-	}
-
+        /* Test assert - REMOVE IN FUTURE LABS */
+        /*
+         * Increase memory allocated for array by 100 chars
+         * until we have eaten it all and print space between Stack and Heap.
+         * Thats how assert works in run-time.
+         */
+        array = realloc( array, (i++ * 100) * sizeof(char));
+        fprintf(stderr, "%d ",
+                (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval));
+        assert(array);
+        /* End test assert */
         /* Set pin 3 low to turn LED off */
         PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
