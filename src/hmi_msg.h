08b019aa98d6e9d810ef6838b925bdd1f606a703
Taavi Tilk <taavi.tilk@gmail.com>
2016-12-15 18:41:51 +0200

    lab03 parnadused

--- src/hmi_msg.h
+++ src/hmi_msg.h
@@ -2,8 +2,9 @@
 #ifndef _HMI_MSG_H_
 #define _HMI_MSG_H_
 #define STUD_NAME "Taavi Tilk" /*jutumärkide vahele enda nimi*/
-#define VER_LIBC "avr-libc version: %S\n"
-#define VER_FW "Version: %s built on: %S %S\n"
+#define VER_LIBC "avr-libc version: %S "
+#define VER_GCC "avr-gcc version: %S\n"
+#define VER_FW "Version: %S built on: %S %S\n"
 #define GET_MONTH "Enter Month mane first letter >"
 #define UPTIME "Uptime: %lu s"
 
